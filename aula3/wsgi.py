65a1f06eb5fbc56cbacba42e917eaa3840933a4d
alisonamerico <alison.americo@gmail.com>
2020-11-10 08:23:33 -0300

    Aula 3 - Introdução a HTTP e ao Flask

---
+++ aula3/wsgi.py
@@ -0,0 +1,17 @@
+# Mínimo funcionamento do Wsgi
+
+# inicio
+def application(environ, start_response):
+    body = b"<h1>Hello world!</h1><button>click</button>"
+    status = "200 OK"
+    headers = [("Content-type", "text/html")]
+    start_response(status, headers)
+    return [body]
+# fim
+
+
+# Simples Wsgi nativo do Python
+if __name__ == "__main__":
+    from wsgiref.simple_server import make_server
+
+    make_server("0.0.0.0", 8000, application).serve_forever()
